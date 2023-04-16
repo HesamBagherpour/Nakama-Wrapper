e1e68a245996913aa977f4b943a91da57dd58d28
Hesam <hesam.hb.bagherpour@gmail.com>
2023-04-16 23:21:52 +0430

    [add] starter asset

---
+++ Assets/NAKAMAKCC.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using StarterAssets;
+using UnityEngine;
+
+public class NAKAMAKCC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isLocalPlayer;
+    public ThirdPersonController thirdPersonController;
+    public StarterAssetsInputs starterAssetsInputs;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangePlayerTransform()
+    {
+        
+    }
+
+    public void SetPlayerTransform()
+    {
+        
+    }
+    
+}
